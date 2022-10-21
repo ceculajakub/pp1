2804c1781ab7a911f3871b25466c466a39d47c78
ceculajakub <ceculajakub@gmail.com>
2022-10-21 19:25:50 +0200

    15 zadan

--- 02-TypesAndVariables/zadanie7.py
+++ 02-TypesAndVariables/zadanie7.py
@@ -1,23 +1,22 @@
-from __future__ import print_function
-
-
-n1, n2, n3, n4, n5 = 5, 1, 6, 8, 3 
-sum = n1+n2+n3+n4+n5
-
-print(n1, n2, n3, n4, n5)
+n1, n2, n3, n4, n5 = 5, 1, 8, 6, 3 
 #a
+sum = n1+n2+n3+n4+n5
 print("Suma wynosi", sum)
 #b
 sum = n1**2 + n2**2 + n3**2 + n4**2 + n5**2
 print(f"Suma kwadratów wynosi {sum}")
 #c
 q = n3/n5
-print(q)
-#d
+print(f"Iloraz n3 i n5 wynosi: {q}")
+#d 1st version
 if n3 == n4:
     print(True) 
 else:
     print(False)
+#d 2nd version
+test = n3 == n4
+print(f"Czy n3 jest równe n4: {test}")
+
 
 
 
--- 02-TypesAndVariables/zadanie9.py
+++ 02-TypesAndVariables/zadanie9.py
@@ -1,3 +1,4 @@
+#Zadanie 9
 imie, nazwisko = input("Podaj imię: "), input("Podaj nazwisko: ")
 
 print(f"{imie} {nazwisko}")
